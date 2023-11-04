ae75138f1a467efbeb66439cbcba2d2113c7c9b8
AleDeso <alessandrodesolda.ad@gmail.com>
2023-11-04 22:46:03 +0100

    cambio posizione lock per i due transfer

--- ClientHandler.java
+++ ClientHandler.java
@@ -63,10 +63,20 @@ public class ClientHandler implements Runnable {
                                     double M = Double.parseDouble(parts[1]);
                                     String aSender = parts[2];
                                     String aReceiver = parts[3];
-
-                                    String mess = transfer(M, aSender, aReceiver);
-
+                                    Account S = a.extract(aSender);
+                                    Account R = a.extract(aReceiver);
+                                    while(S.isLocked()){
+                                            S.wait();
+                                    }
+                                    while(R.isLocked()){
+                                            R.wait();
+                                    }
+                                    S.lock();
+                                    R.lock();
+                                    String mess = transfer(M, S, R);
                                     to.println(mess);
+                                    S.unlock();
+                                    R.unlock();
                                     
                                 }else{
                                      to.println("error not specify details");
@@ -78,7 +88,19 @@ public class ClientHandler implements Runnable {
                                     String aSender = parts[1];
                                     String aReceiver = parts[2];
                                     to.println("Start interattive transation: \n" + "\t Commands: 1.move <money> \t 2.end"); 
-                                    interattive(aSender,aReceiver);
+                                    Account S = a.extract(aSender);
+                                    Account R = a.extract(aReceiver);
+                                    while(S.isLocked()){
+                                            S.wait();
+                                    }
+                                    while(R.isLocked()){
+                                            R.wait();
+                                    }
+                                    S.lock();
+                                    R.lock();
+                                    interattive(S,R);
+                                    S.unlock();
+                                    R.unlock();
                                 } else {
                                     to.println("error not specify details");
                                 }
@@ -117,19 +139,13 @@ public class ClientHandler implements Runnable {
         }
     }
 //////////////////////////IMPLEMENTAZIONE COMANDO TRANSFER
-    public String transfer(double M,String aSender,String aReceiver){
+    public String transfer(double M, Account a_S, Account a_R){
         String message = "";
         try {
             // Decremento del conto mittente
-            Account S = a.extract(aSender);
-            while(S.isLocked()){
-                S.wait();
-            }
-            S.lock();
-
-            if(S.getMoney()>M){
-                S.OutFlow(M);
-                S.setTransation(-M, aSender);
+            if(a_S.getMoney()>=M){
+                a_S.OutFlow(M);
+                a_S.setTransation(-M, a_S.getName());
             }else{
                 throw new Exception("transaction interrupt! -- insufficient balance :(");
             }
@@ -138,19 +154,12 @@ public class ClientHandler implements Runnable {
             
 
             // Incremento del conto ricevente
-            Account R = a.extract(aReceiver);
-            while(R.isLocked()){
-                R.wait();
-            }
-
-            R.lock();
-            R.InFlow(M);
-            R.setTransation(+M, aReceiver);
+            a_R.lock();
+            a_R.InFlow(M);
+            a_R.setTransation(+M, a_R.getName());
 ////////////////////a.add(positiveT.getTKey(), positiveT);//Memorizzo in una HasMap
             
             message = "successful transation";
-            S.unlock();
-            R.unlock();
 
         }catch (Exception e) {   
                     
@@ -162,50 +171,35 @@ public class ClientHandler implements Runnable {
 
         
  //////////////////////////IMPLEMENTAZIONE COMANDO INTERATTIVE
-        public void interattive(String aSender_i, String aReceiver_i){
-            try {
-                Account A1 = a.extract(aSender_i);
-                while(A1.isLocked()){
-                    A1.wait();
-                }
-                A1.lock();
-                Account A2 = a.extract(aReceiver_i);
-                while(A2.isLocked()){
-                    A2.wait();
-                }
-                A2.lock();
-                while (true) {
-                    try{
-                        Scanner from_i = new Scanner(s.getInputStream());
-                        PrintWriter to_i = new PrintWriter(s.getOutputStream(), true); 
-                        String request_i = from_i.nextLine();
-                        String[] parts_i = request_i.split(" ", 2);
-                        String p = parts_i[0].toLowerCase().trim();
-                        switch (p) {
-                            case "move":
-                                if (parts_i.length > 1) {
-                                        double money = Double.parseDouble(parts_i[1]);
-                                        String m = transfer(money, aSender_i, aReceiver_i);
-                                        to_i.println(m);
-                                    } else {
-                                        to_i.println("error not specify details");
-                                    }
-                                break;
-                            case "end":
-                                to_i.println("Finish interattive transation.");
-                                A1.unlock();
-                                A2.unlock();
-                                return; //per uscire dal metodo
-                        
-                            default:
-                                to_i.println("unknown cmd.. try again");
-                        }
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }  
-                }
-            } catch (Exception e) {
-                System.out.println("interattive try per il lock"+e);
+        public void interattive(Account aSender_i, Account aReceiver_i){
+                
+            while (true) {
+                try{
+                    Scanner from_i = new Scanner(s.getInputStream());
+                    PrintWriter to_i = new PrintWriter(s.getOutputStream(), true); 
+                    String request_i = from_i.nextLine();
+                    String[] parts_i = request_i.split(" ", 2);
+                    String p = parts_i[0].toLowerCase().trim();
+                    switch (p) {
+                        case "move":
+                            if (parts_i.length > 1) {
+                                    double money = Double.parseDouble(parts_i[1]);
+                                    String m = transfer(money, aSender_i, aReceiver_i);
+                                    to_i.println(m);
+                                } else {
+                                    to_i.println("error not specify details");
+                                }
+                            break;
+                        case "end":
+                            to_i.println("Finish interattive transation.");
+                            return; //per uscire dal metodo
+                    
+                        default:
+                            to_i.println("unknown cmd.. try again");
+                    }
+                }catch(IOException e){
+                    e.printStackTrace();
+                }  
             }
         }
 }
